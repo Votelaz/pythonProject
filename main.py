0f1e56c343825795eef7cb95437197775734dab5
Votelaz <votelaz1981@gmail.com>
2022-09-18 19:02:46 +0300

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+import string
+
+valid = string.ascii_letters + string.digits + '@' + '.' + '_'
+
+
+def ver(x):
+    if set(x) <= set(valid):
+
+        if x.count("@") == 1 or x.count(".") == 1:
+            print("ДА")
+        else:
+            print("НЕТ")
+    else:
+        print("НЕТ")
+
+
+x = input()
+ver(x)
